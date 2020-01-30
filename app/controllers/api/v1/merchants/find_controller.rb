2f9eb664654df079c1cabceaa67e0e68bcc22407
Ryan Allen <rallenwasd@gmail.com>
2020-01-29 17:19:05 -0700

    Add index to merchants find controller

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::Merchants::FindController < ApplicationController
     merchant = Merchant.find_by(request.query_parameters)
     render json: MerchantSerializer.new(merchant)
   end
+
+  def index
+    merchants = Merchant.where(request.query_parameters)
+    render json: MerchantSerializer.new(merchants)
+  end
 end
