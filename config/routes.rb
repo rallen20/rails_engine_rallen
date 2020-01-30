a2f98fe272984b8b9b9e433bb4b63a02d6c12ab5
Ryan Allen <rallenwasd@gmail.com>
2020-01-29 17:18:51 -0700

    Add merchants namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
+        get '/find_all', to: 'find#index'
         get '/find', to: 'find#show'
+        get '/most_revenue', to: 'most_revenue#index'
       end
       resources :merchants, only: [:index, :show] do
         get '/invoices', to: 'merchant_invoices#index'
