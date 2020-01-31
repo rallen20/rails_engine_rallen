ef70afd8e0ada63a4e5a834ce239fb1c228be50d
Ryan Allen <rallenwasd@gmail.com>
2020-01-31 11:31:11 -0700

    Refactor most revenue from controller to model

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,11 +1,6 @@
 class Api::V1::Merchants::MostRevenueController < ApplicationController
   def index
-    merchants = Merchant.joins(invoices: [:invoice_items, :transactions])
-                        .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
-                        .group(:id)
-                        .where(transactions: {result: 'success'})
-                        .order('total_revenue DESC')
-                        .limit(params[:quantity])
+    merchants = Merchant.most_revenue_variable(params[:quantity])
     render json: MerchantSerializer.new(merchants)
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
+
+  def self.most_revenue_variable(merchant_count)
+    joins(invoices: [:invoice_items, :transactions])
+    .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
+    .group(:id)
+    .where(transactions: {result: 'success'})
+    .order('total_revenue DESC')
+    .limit(merchant_count)
+  end
 end
