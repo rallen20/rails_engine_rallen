6b53af98223acc4535bed45c6f06a63f8b8cdcce
Ryan Allen <rallenwasd@gmail.com>
2020-01-31 09:47:52 -0700

    Refactor query to multiple lines

--- app/controllers/api/v1/merchants/total_revenue_controller.rb
+++ app/controllers/api/v1/merchants/total_revenue_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::Merchants::TotalRevenueController < ApplicationController
   def index
-    total_revenue = Invoice.where(:created_at => Time.zone.parse(params["date"])..Time.zone.parse(params["date"]).next_day).joins(:invoice_items, :transactions).where("transactions.result = 'success'").sum('invoice_items.quantity * invoice_items.unit_price')
+    total_revenue = Invoice.where(:created_at => Time.zone.parse(params["date"])..Time.zone.parse(params["date"]).next_day)
+                           .joins(:invoice_items, :transactions)
+                           .where("transactions.result = 'success'")
+                           .sum('invoice_items.quantity * invoice_items.unit_price')
     total_revenue_object = TotalRevenue.new(total_revenue)
-    render json: TotalRevenueSerializer.new(total_revenue_object).serialized_json
+    render json: TotalRevenueSerializer.new(total_revenue_object)
   end
 end
