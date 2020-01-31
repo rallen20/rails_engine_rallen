457d4b13028e39c3b0cb29b70b1c1d114941bd7b
Ryan Allen <rallenwasd@gmail.com>
2020-01-30 21:47:29 -0700

    Refactor all unit prices to float

--- app/models/total_revenue.rb
+++ app/models/total_revenue.rb
@@ -2,7 +2,7 @@ class TotalRevenue
   attr_accessor :total_revenue, :id
 
   def initialize(total_revenue)
-    @total_revenue = total_revenue
+    @total_revenue = total_revenue.to_s
     @id = 1
   end
 end
