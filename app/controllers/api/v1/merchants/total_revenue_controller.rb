92fbeea1cec0bc715ed0c1807a3304d3550fef3f
Ryan Allen <rallenwasd@gmail.com>
2020-01-30 20:22:38 -0700

    Add merchants total revenue controller

---
+++ app/controllers/api/v1/merchants/total_revenue_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Merchants::TotalRevenueController < ApplicationController
+  def index
+    total_revenue = Invoice.where(:created_at => Time.zone.parse(params["date"])..Time.zone.parse(params["date"]).next_day).joins(:invoice_items, :transactions).where("transactions.result = 'success'").sum('invoice_items.quantity * invoice_items.unit_price')
+    total_revenue_object = TotalRevenue.new(total_revenue)
+    render json: TotalRevenueSerializer.new(total_revenue_object).serialized_json
+  end
+end
