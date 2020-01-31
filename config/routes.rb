831f4a68c65b7f7d5fd9de434a012c38cd335809
Ryan Allen <rallenwasd@gmail.com>
2020-01-31 14:47:17 -0700

    Add resources invoice items

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,18 @@ Rails.application.routes.draw do
         resources :most_revenue, only: [:index]
         resources :revenue, only: [:index]
       end
+
       resources :merchants, only: [:index, :show] do
         get '/invoices', to: 'merchants/invoices#index'
         get '/items', to: 'merchants/items#index'
         get '/favorite_customer', to: 'merchants/favorite_customer#show'
       end
+
       namespace :invoices do
         get '/find_all', to: 'find#index'
         get '/find', to: 'find#show'
       end
+
       resources :invoices, only: [:index, :show] do
         get '/transactions', to: 'invoices/transactions#index'
         get '/items', to: 'invoices/items#index'
@@ -24,6 +27,8 @@ Rails.application.routes.draw do
         get '/customer', to: 'invoices/customer#show'
         get '/merchant', to: 'invoices/merchant#show'
       end
+
+      resources :invoice_items, only: [:index, :show]
     end
   end
 end
