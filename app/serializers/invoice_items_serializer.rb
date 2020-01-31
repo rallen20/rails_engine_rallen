fc941334c2a33b035a5572963f87e0c2bd36ee01
Ryan Allen <rallenwasd@gmail.com>
2020-01-31 16:24:22 -0700

    Refactor invoice items serializer unit_price

--- app/serializers/invoice_items_serializer.rb
+++
@@ -1,8 +0,0 @@
-class InvoiceItemsSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :id, :item_id, :invoice_id, :quantity, :unit_price
-
-  attribute :unit_price do |object|
-    object.unit_price.to_s
-  end
-end
