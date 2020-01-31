26c8b019ba8030dcada2aa0ed9359cf9c1484831
Ryan Allen <rallenwasd@gmail.com>
2020-01-30 20:22:21 -0700

    Refactor namespace merchants revenue

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'find#index'
         get '/find', to: 'find#show'
         get '/most_revenue', to: 'most_revenue#index'
-        get '/revenue', to: 'revenue#index'
+        get '/revenue', to: 'total_revenue#index'
       end
       resources :merchants, only: [:index, :show] do
         get '/invoices', to: 'merchants/invoices#index'
