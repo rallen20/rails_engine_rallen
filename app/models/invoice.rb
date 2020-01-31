0570498270e4ecb7ee3eb916a74a7e1be90a8b4e
Ryan Allen <rallenwasd@gmail.com>
2020-01-31 11:36:01 -0700

    Refactor total revenue from controller to model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,11 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def self.total_revenue_on_specific_date(requested_date)
+    where(:created_at => Time.zone.parse(requested_date)..Time.zone.parse(requested_date).next_day)
+    .joins(:invoice_items, :transactions)
+    .where("transactions.result = 'success'")
+    .sum('invoice_items.quantity * invoice_items.unit_price')
+  end
 end
