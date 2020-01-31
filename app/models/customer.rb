e8fb64ae2886d1bf305544a690e6f2b883410b74
Ryan Allen <rallenwasd@gmail.com>
2020-01-31 12:18:16 -0700

    Add customer favorite_customer method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
-  has_many :invoices  
+  has_many :invoices
+
+  def self.favorite_customer(merchant_id)
+    joins(invoices: :transactions)
+    .select("customers.*, count(transactions.id) as total_transactions")
+    .where("transactions.result = 'success' AND invoices.merchant_id = #{merchant_id}")
+    .group(:id)
+    .order('total_transactions DESC')
+    .limit(1)
+  end
 end
