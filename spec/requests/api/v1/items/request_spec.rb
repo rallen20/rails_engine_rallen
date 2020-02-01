d769e9783bc47f1fda2161f35138babb0985d4f3
Ryan Allen <rallenwasd@gmail.com>
2020-01-31 17:51:48 -0700

    Add tests for items relationship endpoints

--- spec/requests/api/v1/items/request_spec.rb
+++ spec/requests/api/v1/items/request_spec.rb
@@ -23,4 +23,28 @@ describe "Items API" do
     item_parsed = JSON.parse(response.body)
     expect(item_parsed["data"]["id"]).to eq(item.id.to_s)
   end
+
+  it "returns a collection of associated invoice items" do
+    item = create(:item)
+    invoice_items = create_list(:invoice_item, 3, item: item)
+
+    get "/api/v1/items/#{item.id}/invoice_items"
+
+    expect(response).to be_successful
+
+    invoice_items_parsed = JSON.parse(response.body)
+    expect(invoice_items_parsed["data"].count).to eq(3)
+
+  end
+
+  it "returns the associated merchant" do
+    item = create(:item)
+
+    get "/api/v1/items/#{item.id}/merchant"
+
+    expect(response).to be_successful
+
+    merchant_parsed = JSON.parse(response.body)
+    expect(merchant_parsed["data"]["id"]).to eq(item.merchant.id.to_s)
+  end
 end
