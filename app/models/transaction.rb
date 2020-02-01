5eadeb7ef27bb82c3e90854e343d3b8432ac7a01
Ryan Allen <rallenwasd@gmail.com>
2020-01-31 19:27:26 -0700

    Add transaction class method all customers transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+
+  def self.all_customers_transactions(customer_id)
+    joins(:invoice).where("invoices.customer_id = #{customer_id}")
+  end
 end
