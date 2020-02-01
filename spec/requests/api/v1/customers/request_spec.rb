c87e63afa81d84befc36d7068c34f32debe86c42
Ryan Allen <rallenwasd@gmail.com>
2020-01-31 19:26:15 -0700

    Add tests customers relationship endpoints

--- spec/requests/api/v1/customers/request_spec.rb
+++ spec/requests/api/v1/customers/request_spec.rb
@@ -23,4 +23,37 @@ describe "Customers API" do
     customer_parsed = JSON.parse(response.body)
     expect(customer_parsed["data"]["id"]).to eq(customer.id.to_s)
   end
+
+  it "returns a collection of associated invoices" do
+    customer = create(:customer)
+
+    invoices = create_list(:invoice, 3, customer: customer)
+    create_list(:invoice, 2)
+
+    get "/api/v1/customers/#{customer.id}/invoices"
+
+    expect(response).to be_successful
+
+    invoices_parsed = JSON.parse(response.body)
+    expect(invoices_parsed["data"].count).to eq(3)
+  end
+
+  it "returns a collection of associated transactions" do
+    customer = create(:customer)
+    invoice_1 = create(:invoice, customer: customer)
+    invoice_2 = create(:invoice, customer: customer)
+    invoice_3 = create(:invoice)
+
+    create_list(:transaction, 2, invoice: invoice_3)
+
+    create_list(:transaction, 3, invoice: invoice_1)
+    create(:transaction, invoice: invoice_2)
+
+    get "/api/v1/customers/#{customer.id}/transactions"
+
+    expect(response).to be_successful
+
+    transactions_parsed = JSON.parse(response.body)
+    expect(transactions_parsed["data"].count).to eq(4)
+  end
 end
