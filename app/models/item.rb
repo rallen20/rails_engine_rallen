ac2353b36b327cd01a2a7a967ec288f5c9257a0f
Ryan Allen <rallenwasd@gmail.com>
2020-02-01 12:20:18 -0700

    Add items class method most revenue variable

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,13 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.most_revenue_variable(item_count)
+    joins(:invoice_items, invoices: :transactions)
+    .select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
+    .where(transactions: {result: 'success'})
+    .group(:id)
+    .order('total_revenue DESC')
+    .limit(item_count)
+  end
 end
