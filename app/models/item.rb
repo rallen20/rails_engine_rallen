36ae2e61cbf313e074a7665ea85ee81a07701825
Ryan Allen <rallenwasd@gmail.com>
2020-02-02 10:40:44 -0700

    Add best day instance method, only passing one or the other spec harness tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,18 @@ class Item < ApplicationRecord
     .order('total_revenue DESC')
     .limit(item_count)
   end
+
+  def best_day
+    # only 1 or the other tests for this in spec harness will pass
+    # this is an ISSUE
+    invoices
+    .joins(:invoice_items, :transactions)
+    .select("invoices.created_at, SUM(invoice_items.quantity) AS total_quantity")
+    .where(transactions: {result: 'success'})
+    .group("invoices.created_at")
+    .reorder("total_quantity DESC, invoices.created_at DESC")
+    .first
+    .created_at
+    .strftime("%Y-%m-%d")
+  end
 end
